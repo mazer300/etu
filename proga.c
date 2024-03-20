b62266d39b676152dcd877013d17bd4f04d6c5d3
mazer300 <144445631+mazer300@users.noreply.github.com>
2024-03-21 01:36:21 +0300

    Update proga.c

--- proga.c
+++ proga.c
@@ -16,24 +16,24 @@ int RecursionSolution(const char* path, const char* operation){
 		while((d=readdir(dir)) != NULL){
 			if(strcmp(d->d_name, ".")==0 || strcmp(d->d_name, "..")==0) continue;
 
-			char NewPath[100];
+			char NewPath[1024];
 			snprintf(NewPath, sizeof(NewPath), "%s/%s", path, d->d_name);
 			printf("путь -> %s\n",NewPath);
 			if(strcmp(d->d_name,"add")==0) answer=RecursionSolution(NewPath,"add");
 			if(strcmp(d->d_name,"mul")==0) answer=RecursionSolution(NewPath,"mul");
 
-/*			if(strstr(d->d_name,".txt")){
-				int num;
-				FILE *file=fopen(d->d_name,"r");
-				while((fscanf(file,"%d",&num))==1){
-					if(operation){
-						if(strcmp(operation,"add")) answer+=num;
-						if(strcmp(operation,"mul")) answer*=num;
+			if(strstr(d->d_name,".txt")){
+					int num;
+					FILE *file=fopen(NewPath,"r");
+					while((fscanf(file,"%d",&num))==1){
+						if(operation){
+							if(strcmp(operation,"add")) answer+=num;
+							if(strcmp(operation,"mul")) answer*=num;
+						}
 					}
-				}
 				fclose(file);
 			}
-*/			printf("%s\n", d->d_name); // Вывод названия
+			printf("%s\n", d->d_name); // Вывод названия
 		}
 	}
 
